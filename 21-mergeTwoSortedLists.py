c4e142501d41d5e146c484a3b65c22dead92f5e0
Alfred <alfredgarcia14200@gmail.com>
2022-07-13 20:32:22 -0500

    Solution using while

---
+++ 21-mergeTwoSortedLists.py
@@ -0,0 +1,39 @@
+# 21. Merge Two Sorted Lists
+# You are given the heads of two sorted linked lists list1 and list2.
+
+# Merge the two lists in a one sorted list. The list should be made by splicing together the nodes 
+# of the first two lists.
+
+# # Return the head of the merged linked list.
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+
+class ListNode ():
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
+
+class Solution():
+    def mergeTwoLists(self, list1, list2):
+        linkedList = ListNode()
+        cola =  linkedList
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                cola.next = list1
+                list1 = list1.next
+            else:
+                cola.next = list2
+                list2 = list2.next
+            cola = cola.next
+        if list1:
+            cola.next = list1
+        if list2:
+            cola.next = list2
+        return linkedList.next
+
+            
+# Success
+# Details 
+# Runtime: 42 ms, faster than 33.33% of Python online submissions for Merge Two Sorted Lists.
